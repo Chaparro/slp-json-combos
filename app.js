424024dce35d128e5c31a3ecbc7c64a6d9cfd192
Chaparro <ricardoxac@gmail.com>
2019-05-22 01:18:00 -0500

    Added validation to ignore combo objects with wobbling

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ jsonCombo = {
 
 jsonString = '';
 
+function banWobbling(movesArray) { // checks if there are more than 8 grab pummels in combo
+  var pummel = 0;
+  for (var move in movesArray) {
+      if (movesArray[move].moveId == 52){
+        pummel++;
+      }
+  }
+
+  var banned = pummel <= 8 ? true : false;
+  return banned;
+}
+
+//console.log(process.argv[2]);
+
 fs.readdir(testFolder, function(err, items) {
     console.log("Reading " + items.length + " .slp files from directory\n");
     for (var i=0; i<items.length; i++) {
@@ -24,20 +38,23 @@ fs.readdir(testFolder, function(err, items) {
     	const game = new SlippiGame(testFolder + items[i]);
     	console.log(i + ' - ' + items[i]);
  	
-		const settings = game.getSettings();
+		  const settings = game.getSettings();
 
-		const stats = game.getStats();
-		for (var j=0; j<stats.combos.length; j++) {
-			if (stats.combos[j].endPercent - stats.combos[j].startPercent >= 60 && stats.combos[j].didKill == true){
-				console.log(stats.combos[j])
-				comboData = {
-					startFrame : stats.combos[j].startFrame,
-					endFrame : stats.combos[j].endFrame,
-					path : absolutePath
-				};
-				jsonCombo.queue.push(comboData);
-			} 
-		};
+		  const stats = game.getStats();
+  		for (var j=0; j<stats.combos.length; j++) {
+  			if (stats.combos[j].endPercent - stats.combos[j].startPercent >= 60 
+          && stats.combos[j].didKill
+          && banWobbling(stats.combos[j].moves)) {
+
+  				console.log(stats.combos[j]);
+  				comboData = {
+  					startFrame : stats.combos[j].startFrame,
+  					endFrame : stats.combos[j].endFrame,
+  					path : absolutePath
+  				};
+  				jsonCombo.queue.push(comboData);
+  			} 
+  		};
 
     }
     fs.writeFile('./comboData.json', JSON.stringify(jsonCombo), (err) => {
